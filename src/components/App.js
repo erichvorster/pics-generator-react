5acab36024e489422b872374e0ccac53391c4907
erichvorster <erichvorster1@gmail.com>
2022-04-21 19:14:28 +0200

    2nd commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,28 @@
 import React from "react";
+import axios from 'axios';
 import SearchBar from "./SearchBar";
 
-const App = () => {
-    return ( 
-    <div className="ui container" style={{marginTop:'10px'}}>
-        <SearchBar/>
-    </div>);
-};
 
-export default App;
+
+class App extends React.Component {
+
+    onSearchSubmit(term){
+        axios.get('https://api.unsplash.com/search/photos', {
+            params:{query:term},
+            headers:{
+                Authorization: 'Client-ID uGTDcSQ35edwFgzJgCCK0Yh7x2eNHAxh9LJXXfJWZ9I'
+            }
+        })
+         
+    }
+
+render(){
+    return ( 
+        <div className="ui container" style={{marginTop:'10px'}}>
+            <SearchBar functionSubmit={this.onSearchSubmit}/>
+        </div>
+    );
+ }   
+}
+
+export default App; 
